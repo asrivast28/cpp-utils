8ef0882e066d46cc7e1f408ca7b4e9e76e6f3038
Ankit Srivastava <asrivast28@gmail.com>
2021-03-19 19:29:28 -0400

    Created a std::discrete_distribution like class for safely using infinite weights.

--- Random.hpp
+++ Random.hpp
@@ -11,37 +11,62 @@
 
 
 /**
- * @brief Picks an index based on the given weights.
- *        Used for safely handling infinite weights,
- *        since std::discrete_distribution expects finite.
+ * @brief Safe version of std::discrete_distribution
+ *        which handles infinite weights.
  *
  * @tparam IntType Type of integer for picking the index.
- * @tparam InputIt Type of input iterator with weights.
- * @tparam GenType Type of the pseudo random number generator.
- * @param first Iterator to the first weight.
- * @param last Iterator to the last weight.
- * @param generator Instance of the PRNG.
  */
-template <typename IntType, typename InputIt, typename GenType>
-IntType
-discrete_distribution_pick(
-  const InputIt first,
-  const InputIt last,
-  GenType& generator
-)
-{
-  IntType picked = std::distance(first, last);
-  static auto findInf = [] (const double w) { return std::isinf(w); };
-  auto it = std::find_if(first, last, findInf);
-  if (it == last) {
-    auto distrib = std::discrete_distribution<IntType>(first, last);
-    picked = distrib(generator);
+template <typename IntType = int>
+class discrete_distribution_safe {
+public:
+ /**
+  * @brief Constructor for the distribution.
+  *
+  * @tparam InputIt Type of input iterator with weights.
+  * @param first Iterator to the first weight.
+  * @param last Iterator to the last weight.
+  */
+  template <typename InputIt>
+  discrete_distribution_safe(
+    const InputIt first,
+    const InputIt last
+  ) : m_distrib(first, last),
+      m_infiniteIndex(std::numeric_limits<IntType>::max()),
+      m_infiniteWeight(false)
+  {
+    static auto findInf = [] (const double w) { return std::isinf(w); };
+    auto it = std::find_if(first, last, findInf);
+    if (it != last) {
+      m_infiniteIndex = std::distance(first, it);
+      m_infiniteWeight = true;
+    }
   }
-  else {
-    generator.discard(1);
-    picked = std::distance(first, it);
+
+  /**
+   * @brief Returns a random index based on the given weights if all
+   *        the weights are finite. Otherwise, returns the index of
+   *        the first infinite weight. Always moves PRNG state forward.
+   *
+   * @tparam GenType Type of the pseudo random number generator.
+   * @param generator Instance of the PRNG.
+   */
+  template <typename Generator>
+  IntType
+  operator()(Generator& generator)
+  {
+    if (!m_infiniteWeight) {
+      return m_distrib(generator);
+    }
+    else {
+      generator.discard(1);
+      return m_infiniteIndex;
+    }
   }
-  return picked;
-}
+
+private:
+  std::discrete_distribution<IntType> m_distrib;
+  IntType m_infiniteIndex;
+  bool m_infiniteWeight;
+}; // class discrete_distribution_safe
 
 #endif // RANDOM_HPP_
